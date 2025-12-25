deb2049d23783dbe570e8dfd8e4bbb34f9fa52a6
Asilbek2002 <school2924@gmail.com>
2025-12-26 00:03:32 +0500

    adf

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -5,8 +5,7 @@ import QuestionCard from "../QuestionCard/QuestionCard";
 import AnswerList from "../AnswerList/AnswerList";
 import QuestionNavigation from "../QuestioinNavigation/QuestionNavigation";
 
-type Props = {};
-const Test = (props: Props) => {
+const Test = () => {
   const preference: boolean = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
@@ -19,7 +18,6 @@ const Test = (props: Props) => {
     <>
       <Header isDark={isDark} setIsDark={setIsDark} />
       <div className="min-h-screen bg-gray-100 flex justify-center items-center p-6">
-
         <div className="w-full max-w-6xl bg-white rounded-2xl p-6 shadow-lg">
           <div className="grid grid-cols-3 gap-6">
             <QuestionCard />
