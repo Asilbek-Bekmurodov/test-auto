e803dbfb590dc03422c2b71dba48a293bd740554
Asilbek2002 <school2924@gmail.com>
2025-12-27 04:17:51 +0500

    add loader

--- src/pages/public/Public.tsx
+++ src/pages/public/Public.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Carousel from "./components/Carousel/Carousel";
 import FAQ from "./components/Faq/Faq";
 import Footer from "./components/Footer/Footer";
@@ -5,8 +6,28 @@ import Hero from "./components/Hero/Hero";
 import How from "./components/How/How";
 import PublicHeader from "./components/PublicHeader/PublicHeader";
 import TestimonialSlider from "./components/TestimonialSlider/TestimonialSlider";
+import BoxLoader from "../../components/Loaders/BoxLoader/BoxLoader"; // loader component
 
 const Public = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulyatsiya uchun 1.5s delay, real backend fetch ishlatilsagina fetch yakunlangandan keyin setLoading(false)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-[100vh] flex items-center justify-center">
+        <BoxLoader />
+      </div>
+    );
+  }
+
   return (
     <>
       <PublicHeader />
@@ -21,4 +42,5 @@ const Public = () => {
     </>
   );
 };
+
 export default Public;
