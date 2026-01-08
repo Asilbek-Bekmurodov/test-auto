5d008635c51beb5906489c727c852c3e4ca16fa5
Asilbek2002 <school2924@gmail.com>
2026-01-09 00:17:42 +0500

    fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,6 @@ import { useTheme } from "../../context/ThemeContext";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { useState } from "react";
 import { NewsGrid } from "../../components/News/NewsGrid";
-import Problems from "../../components/Problems/Problems";
 import ShowProblems from "../../components/Problems/ShowProblems";
 
 function Home() {
